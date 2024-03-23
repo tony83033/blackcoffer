e18c181147e54a435bd5f23351dea336f678839e
Sumit pal <tonystark83033@gmail.com>
2024-03-23 13:54:57 +0530

    core:[commit without prettier]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { cn } from "@/lib/utils"
 const inter = Inter({ subsets: ["latin"] });
-
+import { Inter as FontSans } from "next/font/google"
 export const metadata: Metadata = {
   title: "BlackCoffer",
   description: "Develop by Sumit pal sumitpal.wiki",
 };
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+ 
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import BarChart from "./components/barChart";
 export default function Home() {
-  return <h1>Hii bro</h1>;
+  return <div className="bg-white p-8">
+  <div className="grid grid-cols-3 gap-8">
+    <div className="col-span-1">
+      <div className="mb-8">
+        <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+      </div>
+      <div>
+      <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+      </div>
+    </div>
+    <div className="col-span-2">
+      <div className="mb-8">
+      <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-8">
+        <div>
+        <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+        </div>
+        <div>
+        <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+        </div>
+        <div>
+        <div className="w-full h-[300px]">
+        <BarChart  />
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 }
